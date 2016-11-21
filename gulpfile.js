33486a8c5850dfb7414049bc4684bd789d723793
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-11-22 01:45:10 +0800

    Add angular and gulp-jshint to dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    jshint = require('gulp-jshint');
  
 gulp.task('hello', function(){
     console.log('Hello Gulp.js');
