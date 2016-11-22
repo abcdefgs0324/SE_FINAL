5a0d7e059a4f5eb336bdfe4de110e37611d85bb9
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-11-22 22:50:29 +0800

    Init index.jade & set gulp jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
+    jade = require('gulp-jade'),
     jshint = require('gulp-jshint');
  
-gulp.task('hello', function(){
+gulp.task('hello', function() {
     console.log('Hello Gulp.js');
+});
+
+gulp.task('jade', function() {
+    return gulp.src('client/views/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('client/'));
 });
 
