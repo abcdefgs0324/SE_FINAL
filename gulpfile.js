60bb392090b9f5b6267ca510ffc3b60451812033
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-11-23 14:29:20 +0800

    Set 'gulp serve' and minor update for index.jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
     jade = require('gulp-jade'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    exec = require('child_process').exec;
  
+
 gulp.task('hello', function() {
     console.log('Hello Gulp.js');
 });
@@ -15,3 +17,17 @@ gulp.task('jade', function() {
         .pipe(gulp.dest('client/'));
 });
 
+gulp.task('jshint', function() {
+    return gulp.src('client/app/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('serve', function(cb) {
+    exec('node client/app.js', function(err) {
+        if (err) {
+            return cb(err);
+        }
+        cb();
+    });
+});
