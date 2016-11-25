654fabc1e4bf7603bc7d78c9e8d7566939acc310
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-11-26 05:03:58 +0800

    Add automation for sass, jade, and watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,15 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     jade = require('gulp-jade'),
     jshint = require('gulp-jshint'),
-    exec = require('child_process').exec;
+    exec = require('child_process').exec,
+    sass = require('gulp-sass');
  
 
 gulp.task('hello', function() {
     console.log('Hello Gulp.js');
 });
 
-gulp.task('jade', function() {
+gulp.task('jadeIndex', function() {
     return gulp.src('client/views/*.jade')
         .pipe(jade({
             pretty: true
@@ -17,10 +18,30 @@ gulp.task('jade', function() {
         .pipe(gulp.dest('client/'));
 });
 
+gulp.task('jadeOther', function() {
+    return gulp.src('client/views/*/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('client/app/'));
+});
+
 gulp.task('jshint', function() {
     return gulp.src('client/app/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('styles', function() {
+    return gulp.src('client/static/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('client/static/css/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('client/static/scss/*.scss', ['styles']);
+    gulp.watch('client/views/*.jade', ['jadeIndex']);
+    gulp.watch('client/views/*/*.jade', ['jadeOther']);
 });
 
 gulp.task('serve', function(cb) {
@@ -31,3 +52,8 @@ gulp.task('serve', function(cb) {
         cb();
     });
 });
+
+gulp.task('jade', ['jadeIndex', 'jadeOther']);
+gulp.task('run', ['jadeIndex', 'jadeOther', 'serve']);
+
+
