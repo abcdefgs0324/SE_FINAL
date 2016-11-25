372893c5013f859a20bf5e10304307a836e0f6c8
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-11-26 02:19:46 +0800

    Seperate html and css into two files

--- client/app.js
+++ client/app.js
@@ -12,6 +12,7 @@ app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/function', express.static(__dirname + '/function'));
 app.use('/components', express.static(__dirname + '/components'));
+app.use('/static', express.static(__dirname + '/static'));
 
 http.listen(process.env.PORT || 3000, function() {
     console.log('listening on *:3000');
