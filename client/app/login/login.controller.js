e31ef722fa2976aed86f35e42c03d1574c42e2ef
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-11-26 04:09:33 +0800

    Update for login page

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -2,6 +2,7 @@
 
 angular.module('policeApp')
     .controller('LoginCtrl', [ '$scope', 'transfer', function ($scope, transfer) {
+        $scope.logging = false;
 
         $scope.goApp = function (targetPage) {
             transfer.go(targetPage);
--- client/app/login/login.js
+++ client/app/login/login.js
@@ -5,11 +5,6 @@ angular.module('policeApp')
         $stateProvider
             .state('login', {
                 templateUrl: 'app/login/login.html',
-                controller: 'LoginCtrl',
-                    xtemplateProvider: function ($timeout, transfer) {
-                        return $timeout(function () {
-                            return transfer.go('app/login/login.html', $timeout);
-                        }, 100);
-                    }
+                controller: 'LoginCtrl'
             });
     });
