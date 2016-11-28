dabb559e5a7a4b920d15167e7b18a4bcab629892
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-11-28 20:38:12 +0800

    Add code for sample template of report page

--- client/app/report/report.controller.js
+++ client/app/report/report.controller.js
@@ -3,6 +3,30 @@
 angular.module('policeApp')
     .controller('ReportCtrl', [ '$scope', 'transfer', function ($scope, transfer) {
 
+        $scope.tabSelected = 0;
+        $scope.twoDisabled = true;
+        $scope.threeDisabled = true;
+        $scope.fourDisabled = true;
+
+        $scope.stepOne = function() {
+            $scope.tabSelected = 0;
+        };
+
+        $scope.stepTwo = function() {
+            $scope.tabSelected = 1;
+            $scope.twoDisabled = false;
+        };
+
+        $scope.stepThree = function() {
+            $scope.tabSelected = 2;
+            $scope.threeDisabled = false;
+        };
+
+        $scope.stepFour = function() {
+            $scope.tabSelected = 3;
+            $scope.fourDisabled = false;
+        };
+
         $scope.goApp = function (targetPage) {
             transfer.go(targetPage);
         };
