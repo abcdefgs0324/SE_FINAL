29661994c3fb306faeee101e0edbc74c34cb31f2
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-12-02 11:19:44 +0800

    Add report setting and it will be sent to blockchain while connecting

--- client/app/report/report.controller.js
+++ client/app/report/report.controller.js
@@ -7,7 +7,23 @@ angular.module('policeApp')
         $scope.twoDisabled = true;
         $scope.threeDisabled = true;
         $scope.fourDisabled = true;
+
+        $scope.id = "";
+        $scope.recordNum = "";
+        $scope.myPlate = "";
+        $scope.otherPlate = "";
+        $scope.place = "";
         $scope.date = new Date();
+        $scope.time = "";
+        $scope.otherBehavior = "";
+        $scope.myBehavior = "";
+        $scope.speed = "";
+        $scope.road= "";
+        $scope.sign = "";
+        $scope.broken = "";
+        $scope.otherCond = "";
+        $scope.photo = "";
+        $scope.notes = "";
 
         $scope.stepOne = function() {
             $scope.tabSelected = 0;
@@ -26,6 +42,40 @@ angular.module('policeApp')
         $scope.stepFour = function() {
             $scope.tabSelected = 3;
             $scope.fourDisabled = false;
+        };
+
+        $scope.done = function() {
+            var settings = {
+                id: $scope.id,
+                recordNum: $scope.recordNum,
+                identity: {
+                    myPlate: $scope.myPlate,
+                    otherPlate: $scope.otherPlate
+                },
+                condition: {
+                    date: $scope.date,
+                    time: $scope.time,
+                    place: $scope.place,
+                    otherBehavior: $scope.otherBehavior,
+                    myBehavior: $scope.myBehavior,
+                    speed: $scope.speed
+                },
+                extFactor: {
+                    road: $scope.road,
+                    sign: $scope.sign
+                },
+                result: {
+                    broken: $scope.broken,
+                    otherCond: $scope.otherCond
+                },
+                otherData: {
+                    photo: $scope.photo,
+                    notes: $scope.notes
+                }
+            };
+
+            console.log('done');
+            console.log(settings);
         };
 
         $scope.goApp = function (targetPage) {
