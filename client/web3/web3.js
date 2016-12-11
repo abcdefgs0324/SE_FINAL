102ef7b2eaa360719f4c9f037de87f432838a4f3
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-12-11 19:16:47 +0800

    Fix for unlock the account and bumbcase function call

--- client/web3/web3.js
+++ client/web3/web3.js
@@ -2,4 +2,6 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
+web3.personal.unlockAccount('0x988718f785847d8257287ef9758aa63d3d62268f', '<PASSWORD>');
+
 module.exports = web3;
