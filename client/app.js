881e4ba28aaa11341a2d51b9ccdb1c604c910e04
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-12-17 15:54:59 +0800

    Modify for getting data and posting a new contract with the correct user and setting

--- client/app.js
+++ client/app.js
@@ -12,7 +12,8 @@ var eventEmitter = require('./web3/eventEmitter.js');
 var web3 = require('./web3/web3.js');
 var eth = web3.eth;
 // import 同目錄的 bumbcase
-var bumbcase = require('./web3/BumbCase.js');
+var bumbcaseModule = require('./web3/BumbCase.js');
+var bumbcaseAbi = bumbcaseModule.bumbcaseAbi;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -23,69 +24,55 @@ app.use('/app', express.static(__dirname + '/app'));
 app.use('/function', express.static(__dirname + '/function'));
 app.use('/components', express.static(__dirname + '/components'));
 app.use('/static', express.static(__dirname + '/static'));
+app.use('/web3', express.static(__dirname + '/web3'));
 
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-/*
-http.listen(process.env.PORT || 3000, function() {
-    console.log('listening on *:3000');
-});
-*/
 http.listen(3000);
 
 
-app.get('/userdata', function(req, res){
+app.post('/userdata', function(req, res){
     var user = web3.eth.accounts[0];
+    var bumbcaseCon = web3.eth.contract(bumbcaseAbi);
+    var bumbcase = bumbcaseCon.at(req.body.address);
     res.json({
-        id: bumbcase.returnid,
-        recordNum: bumbcase.returnrecordNum,
+        id: bumbcase.returnid.call(),
+        recordNum: bumbcase.returnrecordNum.call(),
         identity: {
-            myPlate: bumbcase.returnmyPlate,
-            otherPlate: bumbcase.returnotherPlate
+            myPlate: bumbcase.returnmyPlate.call(),
+            otherPlate: bumbcase.returnotherPlate.call()
         },
         condition: {
-            date: bumbcase.returndate,
-            time: bumbcase.returntime,
-            place: bumbcase.returnplace,
-            otherBehavior: bumbcase.returnotherBehavior,
-            myBehavior: bumbcase.returnmyBehavior,
-            speed: bumbcase.returnspeed
+            date: bumbcase.returndate.call(),
+            time: bumbcase.returntime.call(),
+            place: bumbcase.returnplace.call(),
+            otherBehavior: bumbcase.returnotherBehavior.call(),
+            myBehavior: bumbcase.returnmyBehavior.call(),
+            speed: bumbcase.returnspeed.call()
         },
         extFactor: {
-            road: bumbcase.returnroad,
-            sign: bumbcase.returnsign
+            road: bumbcase.returnroad.call(),
+            sign: bumbcase.returnsign.call()
         },
         result: {
-            broken: bumbcase.returnbroken,
-            otherCond: bumbcase.returnotherCond
+            broken: bumbcase.returnbroken.call(),
+            otherCond: bumbcase.returnotherCond.call()
         },
         otherData: {
-            photo: bumbcase.returnphoto,
-            notes: bumbcase.returnnotes
+            photo: bumbcase.returnphoto.call(),
+            notes: bumbcase.returnnotes.call()
         }
     });
 });
 
 app.post('/postdata', function(req, res) {
-    var user = web3.eth.accounts[0];
-    bumbcase.NewCase(user, req.body.identity.myPlate, req.body.identity.otherPlate, req.body.condition, req.body.condition.date, req.body.condition.time, req.body.condition.place, req.body.condition.otherBehavior, req.body.condition.myBehavior, req.body.condition.speed, req.body.extFactor.road, req.body.extFactor.sign, req.body.result.broken, req.body.result.otherCond, req.body.otherData.photo, req.body.otherData.notes, {
-        from: user,
-        gas: 4600000
-    }, function(err, txhash) {
-        if(!err) {
-            eventEmitter.once('NewCaseEvent:' + user, function(eventPayload) {
-                eventPayload['txhash'] = txhash;
-                res.json(eventPayload);
-            })
-        }else {
-            console.log(err);
-            res.status(500).json(err);
-        }
-    })
-
+    var user = 0;
+    var bumbcaseCon = web3.eth.contract(bumbcaseAbi);
+    var bumbcase = bumbcaseCon.at(req.body.address);
+    bumbcase.NewCase(user, req.body.identity.myPlate, req.body.identity.otherPlate, req.body.condition.date, req.body.condition.time, req.body.condition.place, req.body.condition.otherBehavior, req.body.condition.myBehavior, parseInt(req.body.condition.speed), req.body.extFactor.road, req.body.extFactor.sign, req.body.result.broken, req.body.result.otherCond, req.body.otherData.photo, req.body.otherData.notes, {from:eth.accounts[0], gas:'470000'});
 });
 
 exports = module.exports = app;
