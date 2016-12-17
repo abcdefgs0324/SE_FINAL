e7f1de7cfbd25fa0c4fa54dc69c942a580efd9ef
abcdefgs0324 <abcdefgs0324@gmail.com>
2016-12-17 15:54:24 +0800

    Send the login user's address to get his history data

--- client/app/record/record.controller.js
+++ client/app/record/record.controller.js
@@ -1,25 +1,30 @@
+/*global nowUser:true*/
+
 'use strict';
 
 angular.module('policeApp')
     .controller('RecordCtrl', [ '$scope', '$http', 'transfer', function ($scope, $http, transfer) {
 
-        $scope.user = {'id': '11111'};
         $scope.settings = null;
 
-        $http({
-            url: '/userdata',
-            method: 'GET',
-            data: $scope.user
-        })
-        .success(function (res) {
-            console.log('res:');
-            console.log(res);
-            $scope.settings = res;
-        })
-        .error(function (e) {
-            console.log('Error:');
-            console.log(e);
-        });
+        $scope.getData = function () {
+            $http({
+                url: '/userdata',
+                method: 'POST',
+                data: nowUser
+            })
+            .success(function (res) {
+                console.log('res:');
+                console.log(res);
+                $scope.settings = res;
+            })
+            .error(function (e) {
+                console.log('Error:');
+                console.log(e);
+            });
+        };
+
+        $scope.getData();
 
         $scope.goApp = function (targetPage) {
             transfer.go(targetPage);
