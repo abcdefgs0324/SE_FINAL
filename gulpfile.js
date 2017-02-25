53f7713c768cc4eb74bae32ccab7c8f6317bd14e
abcdefgs0324 <abcdefgs0324@gmail.com>
2017-02-25 16:50:50 +0800

    Added pump package and modify gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,39 +3,52 @@ var gulp = require('gulp'),
     pug = require('gulp-pug'),
     jshint = require('gulp-jshint'),
     exec = require('child_process').exec,
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    pump = require('pump');
  
 
 gulp.task('hello', function() {
     console.log('Hello Gulp.js');
 });
 
-gulp.task('pugIndex', function() {
-    return gulp.src('client/views/*.pug')
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(gulp.dest('client/'));
+gulp.task('pugIndex', function(cb) {
+    pump([
+            gulp.src('client/views/*.pug'),
+            pug({ pretty: true }),
+            gulp.dest('client/')
+        ],
+        cb
+    );
 });
 
-gulp.task('pugOther', function() {
-    return gulp.src('client/views/*/*.pug')
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(gulp.dest('client/app/'));
+gulp.task('pugOther', function(cb) {
+    pump([
+            gulp.src('client/views/*/*.pug'),
+            pug({ pretty: true }),
+            gulp.dest('client/app/')
+        ],
+        cb
+    );
 });
 
-gulp.task('lint', function() {
-    return gulp.src('client/app/**/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'));
+gulp.task('lint', function(cb) {
+    pump([
+            gulp.src('client/app/**/*.js'),
+            jshint(),
+            jshint.reporter('jshint-stylish')
+        ],
+        cb
+    );
 });
 
-gulp.task('styles', function() {
-    return gulp.src('client/static/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('client/static/css/'));
+gulp.task('styles', function(cb) {
+    pump([
+            gulp.src('client/static/scss/*.scss'),
+            sass(),
+            gulp.dest('client/static/css/')
+        ],
+        cb
+    );
 });
 
 gulp.task('watch', function() {
